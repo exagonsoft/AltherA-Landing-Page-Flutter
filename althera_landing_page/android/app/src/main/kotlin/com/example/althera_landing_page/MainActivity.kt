289c11070249d07c873c386602590a04ab4eac96
Alvaro R Martin <exagonsoft@gmail.com>
2023-05-23 02:57:22 -0400

    #1 Crete the Flutter App
    Recreate the Application using Stacked Template

---
+++ althera_landing_page/android/app/src/main/kotlin/com/example/althera_landing_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.althera_landing_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ althera_landing_page/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:althera_landing_page/app/app.bottomsheets.dart';
+import 'package:althera_landing_page/app/app.dialogs.dart';
+import 'package:althera_landing_page/app/app.locator.dart';
+import 'package:althera_landing_page/app/app.router.dart';
+import 'package:althera_landing_page/ui/common/app_colors.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+void main() {
+  setupLocator();
+  setupDialogUi();
+  setupBottomSheetUi();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: Theme.of(context).copyWith(
+        primaryColor: kcBackgroundColor,
+        focusColor: kcPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+            ),
+      ),
+      initialRoute: Routes.startupView,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      navigatorKey: StackedService.navigatorKey,
+      navigatorObservers: [
+        StackedService.routeObserver,
+      ],
+    );
+  }
+}
